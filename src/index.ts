0e77851b854c007f15b36b1e28218e769c9f9e01
DylDev <dylanb92010@gmail.com>
2026-05-02 22:37:07 -0400

    Fix slash commands

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits, REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 import config from '../config.json' with { type: 'json' };
-import path from 'node:path/win32';
 import setPingResponseMsg from './commands/util/setPingResponseMsg.js';
 
 dotenv.config();
 const token = config.DISCORD_TOKEN;
 //const commandFiles = path.join(__dirname, 'commands', 'util');
 
-
 const comm = new Collection();
-
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -20,17 +17,38 @@ const client = new Client({
 });
 
 
-client.once('clientReady', () => {
+client.once('ready', async () => {
     console.log(`Logged in as ${client.user?.tag}!`);
+
+    const rest = new REST({ version: '10' }).setToken(token);
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        const commands = [
+            setPingResponseMsg.data.toJSON(),
+        ];
+
+        if (client.user) {
+            await rest.put(
+                Routes.applicationCommands(client.user.id),
+                { body: commands }
+            );
+        }
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 });
 
-// Basic message response
+// Basic ping-pong message response
 client.on('messageCreate', (message) => {
     if (message.content === '!ping') {
         message.reply('Pong!');
     }
 });
 
+// Set ping response message slash command
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     if (interaction.commandName === 'set-ping-response') {
